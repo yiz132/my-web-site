806b924d658fd8f4c537d49ac2336cbb288f622d
J.Z <yiz132@pitt.edu>
2020-01-09 17:51:57 -0500

    Updates

---
+++ precache-manifest.62668237f5180fc80f96978ff6e0527e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82288ff28ef992591c3c3c6454a4d5b9",
+    "url": "/my-web-site/index.html"
+  },
+  {
+    "revision": "dd0323a8d19c87866b64",
+    "url": "/my-web-site/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "972c3e45fe93cb762ad8",
+    "url": "/my-web-site/static/js/2.3f28a4a9.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "/my-web-site/static/js/2.3f28a4a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd0323a8d19c87866b64",
+    "url": "/my-web-site/static/js/main.883f8caa.chunk.js"
+  },
+  {
+    "revision": "f162d0cd12b3a8d58fb9",
+    "url": "/my-web-site/static/js/runtime-main.50ae7b84.js"
+  }
+]);

8cf24c1ef6881f2a5c8f13bf032cc7c3fc222a50
J.Z <yiz132@pitt.edu>
2020-01-09 23:30:17 -0500

    photo

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2020 Yixuan Zhang</li>
-              <li>Modified from <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Tamplate from <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
         </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,9 +5,7 @@ class Header extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var occupation= this.props.data.occupation;
       var description= this.props.data.description;
-      var city= this.props.data.address.city;
       var networks= this.props.data.social.map(function(network){
         return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
       })
@@ -26,7 +24,7 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
+            <li><a className="smoothscroll" href="#testimonials">Hobbies</a></li>
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
          </ul>
 
@@ -34,8 +32,10 @@ class Header extends Component {
 
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">I'm {name}.</h1>
-            <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
+            <h1 className="responsive-headline">Welcome to</h1>
+            <h1 className="responsive-headline">{name}'s</h1>
+            <h1 className="responsive-headline">Home</h1>
+            <h3>{description}.</h3>
             <hr />
             <ul className="social">
                {networks}
